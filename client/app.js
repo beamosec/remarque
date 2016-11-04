a87d67d8700cc0a0fcc73d8a515663af063cc80f
beamosec <beamosec@gmail.com>
2016-11-03 21:30:25 -0400

    insta auth and bulk commit

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-angular.module('Instagram', ['ngRoute', 'ngMessages'])
+angular.module('Instagram', ['ngRoute', 'ngMessages', 'satellizer'])
   .config(function($routeProvider) {
     $routeProvider
   .when('/', {
@@ -18,4 +18,17 @@ angular.module('Instagram', ['ngRoute', 'ngMessages'])
     controller: 'DetailCtrl'
   })
   .otherwise('/');
+
+$authProvider.loginUrl = 'http://localhost:3000/auth/login';
+$authProvider.signupUrl = 'http://localhost:3000/auth/signup';
+$authProvider.oauth2({
+  name: 'instagram',
+  url: 'http://localhost:3000/auth/instagram',
+  redirectUri: 'http://localhost:8000',
+  clientId: '650c467538d84dd59f8caeeee1286a21',
+  requiredUrlParams: ['scope'],
+  scope: ['likes'],
+  scopeDelimiter: '+',
+  authorizationEndpoint: 'https://api.instagram.com/oauth/authorize'
   });
+});
