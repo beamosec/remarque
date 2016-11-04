2aad3352d871dd8d44aafe82bff9ed3d4462ab38
beamosec <beamosec@gmail.com>
2016-11-03 23:22:44 -0400

    login page and node express server

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: process.env.db || 'localhost',
+  clientSecret: process.env.clientSecret || 'a3d5810986974e7db27003a5658a5ad4',
+  tokenSecret: process.env.tokenSecret || 'difficult string'
+};
